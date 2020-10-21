5b0445f514a652ef6f80c1eacea16495e201c263
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-20 21:51:09 -0300

    added another validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     has_many :comments
     validates :title, presence: true,
                         length: {minimum: 5}
+    validates :title, format: { with: /\D/, message: 'it cant be mumbers' }
 end
